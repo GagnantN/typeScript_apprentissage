abdea64a43dbffd92b3f6402a7fe58175becd6cb
Nicolas <gagnantnicolas64@gmail.com>
2025-11-13 09:25:50 +0100

    Finir exerce 1 du niveau 2

---
+++ Niveau02/Ex11/exercice11.js
@@ -0,0 +1,6 @@
+var etudiant = {
+    nom: "Gagnant",
+    age: 30,
+    adresse: "27 rue Parra d'Andert, 38180 Seyssins"
+};
+console.log(etudiant);
---
+++ Niveau02/Ex11/exercice11.ts
@@ -0,0 +1,13 @@
+interface Personne {
+    nom: string;
+    age: number;
+    adresse: string;
+}
+
+const etudiant = {
+    nom: "Gagnant",
+    age: 30,
+    adresse: "27 rue Parra d'Andert, 38180 Seyssins"
+}
+
+console.log(etudiant)
